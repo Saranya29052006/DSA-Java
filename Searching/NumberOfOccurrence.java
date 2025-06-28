9bf3bc43755bbe3172aaa9bd9e933e5bcd0edfe2
Saranya <saranya29052006@gmail.com>
2025-06-28 22:04:48 +0530

    Create NumberOfOccurrence.java

---
+++ Searching/NumberOfOccurrence.java
@@ -0,0 +1,30 @@
+public class Solution {
+    public static int count(int arr[], int n, int x) {
+        int low=0;
+        int high=n-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]>=x){
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        int lb=low;
+        low=0;
+        high=n-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]>x){
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        int ub=low;
+        if(lb==n || arr[lb]!=x){
+            return 0;
+        }
+        return ub-lb;
+    }
+}
